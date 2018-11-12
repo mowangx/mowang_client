825cb9905a5b3a8d51a3831666209905409db285
mowangx <36142663+mowangx@users.noreply.github.com>
2018-11-12 18:30:46 +0800

    #refactor 12

--- english_word/assets/scripts/ui/select.ts
+++ english_word/assets/scripts/ui/select.ts
@@ -20,9 +20,18 @@ export default class select extends cc.Component {
     @property(cc.Prefab)
     num_1: cc.Prefab = null;
 
+    @property(cc.Node)
+    lvl_node: cc.Node = null;
+
+    private click_x: Number = 0;
+    private click_y: Number = 0;
+
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        this.lvl_node.on('touchend', this.on_touch_end, this);
+        this.lvl_node.on('touchcancel', this.on_touch_end, this);
+    },
 
     start () {
         for (let i=0; i<this.grid_list.length; ++i) {
@@ -35,9 +44,35 @@ export default class select extends cc.Component {
             replace_node.height = grid.height;
 
         }
-    }
+    },
 
     // update (dt) {}
+
+    on_touch_end(event): void {
+        this.click_x = this.lvl_node.convertTouchToNodeSpaceAR(event).x;
+        this.click_y = this.lvl_node.convertTouchToNodeSpaceAR(event).y;
+        this.check_click();
+    },
+
+    check_click(): void {
+        let len = this.grid_list.length;
+        for (let i = 0; i < len; i ++) {
+            let grid = this.grid_list[i];
+            let start_x = grid.x - grid.width / 2;
+            let start_y = grid.y - grid.height / 2;
+            let end_x = grid.x + grid.width / 2;
+            let end_y = grid.y + grid.height / 2;
+            if (start_x < this.click_x && end_x > this.click_x &&
+                start_y < this.click_y && end_y > this.click_y) {
+                this.on_click_grid(i);
+                return;
+            }
+        }
+    },
+
+    on_click_grid(i: number): void {
+
+    },
 
     get_num_prefab(idx: number): cc.Prefab {
         return this.num_1;
